ad2bd8f515bc7146ea030a028ee2b94618e95b0b
Tusk <80130306+Tusk-Drive@users.noreply.github.com>
2023-05-06 13:14:01 -0500

    Add files via upload

---
+++ web-wether-scraper.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL of the website to scrape
+url = 'https://weather.com/'
+
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the element containing the weather information
+weather_element = soup.find('div', {'class': 'weather'})
+
+# Extract the text of the weather information
+weather_text = weather_element.text.strip()
+
+# Print the weather information
+print("Most Recent Weather:", weather_text)
