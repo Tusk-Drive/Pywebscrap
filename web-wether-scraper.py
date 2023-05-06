557ad4c39eef69de7290dc089e90e39d59804f0c
Tusk-Drive <rectornolan@gmail.com>
2023-05-06 13:59:16 -0500

    modified:   web-wether-scraper.py

--- web-wether-scraper.py
+++ web-wether-scraper.py
@@ -1,20 +1,41 @@
-from requests import get
+import requests
 from bs4 import BeautifulSoup
-#This is a collaborative event to get a web scraper to pull publicly known information to present in the code.
+import tkinter as tk
+
+# Function to scrape website and extract desired information
+def scrape_website(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the element(s) containing the desired information
+    # Modify this part based on the structure of the website and the information you want to scrape
+    # Example: Extracting the titles of articles from a news website
+    article_titles = soup.find_all('h2', class_='article-title')
+
+    # Process the scraped information
+    titles = [title.text.strip() for title in article_titles]
+    return titles
+
 # URL of the website to scrape
-URL = 'https://openweathermap.org/city'
+url = 'https://www.example.com'
 
-# Send a GET request to the URL
-response = get(URL)
+# Call the scrape_website function with the URL
+scraped_data = scrape_website(url)
 
-# Create a BeautifulSoup object
-soup = BeautifulSoup(response.text, 'html.parser')
+# Create a pop-up window
+root = tk.Tk()
+root.title("Scraped Data")
 
-# Find the element containing the weather information
-weather_element = soup.find('div', {'class': 'weather__temperature'})
+# Create a text widget to display the scraped data
+text_widget = tk.Text(root, height=10, width=50)
+text_widget.pack()
 
-# Extract the text of the weather information
-weather_text = weather_element.text.strip()
+# Insert the scraped data into the text widget
+for data in scraped_data:
+    text_widget.insert(tk.END, data + '\n')
 
-# Print the weather information
-print("Most Recent Weather:", weather_text)
+# Start the Tkinter event loop
+root.mainloop()
